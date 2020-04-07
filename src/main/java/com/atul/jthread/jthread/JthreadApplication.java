4393e6e168be4c2a0a38b3309367c7df99c31ad8
Atul Kumar <atulkumar458@gmail.com>
2020-04-07 19:42:43 +0530

    Initial ccommit

---
+++ src/main/java/com/atul/jthread/jthread/JthreadApplication.java
@@ -0,0 +1,13 @@
+package com.atul.jthread.jthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JthreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JthreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atul/jthread/jthread/JthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.jthread.jthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JthreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
